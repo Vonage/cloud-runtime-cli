44de7c88937a0aadb04ac5cedefc6eba32622ec4
dacheng293 <da.cheng@vonage.com>
2024-04-05 09:40:43 -0400

    fix: lint issue
    Signed-off-by: dacheng293 <da.cheng@vonage.com>

--- pkg/config/manifest.go
+++ pkg/config/manifest.go
@@ -34,8 +34,8 @@ type Project struct {
 }
 
 type Env struct {
-	Name   string `json:"name"  yaml:"name"`
-	Value  string `json:"value" yaml:"value"`
+	Name   string `json:"name"   yaml:"name"`
+	Value  string `json:"value"  yaml:"value"`
 	Secret string `json:"secret" yaml:"secret"`
 }
 
