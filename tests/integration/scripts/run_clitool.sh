eff1bb7998f3fcee1f9f5314e932533f761af9fb
dacheng293 <da.cheng@vonage.com>
2024-04-15 18:04:39 -0400

    fix: intergration test
    Signed-off-by: dacheng293 <da.cheng@vonage.com>

--- tests/integration/scripts/run_clitool.sh
+++ tests/integration/scripts/run_clitool.sh
@@ -4,7 +4,7 @@ sleep 30
 
 output=$(./vcr-cli deploy --config-file /app/tests/integration/testdata/config.yaml -f /app/tests/integration/testdata/vcr.yaml)
 
-if echo "$output" | grep -q "✓ Instance has been deployed!"; then
+if echo "$output" | grep -q "Instance has been deployed!"; then
     echo "Success detected in output."
     exit 0
 elif echo "$output" | grep -q "failed"; then
