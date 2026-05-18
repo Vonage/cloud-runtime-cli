081f338f3fe9248bb5964e948f1e2e75d1a69e13
Gorka Revilla <gorka.revillafernandez@vonage.com>
2026-05-18 09:37:23 +0200

    test(app): add tests for vcr app delete command [APIAPEX-2823]

---
+++ vcr/app/delete/delete_test.go
@@ -0,0 +1,152 @@
+package delete
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"testing"
+
+	"github.com/cli/cli/v2/pkg/iostreams"
+	"github.com/golang/mock/gomock"
+	"github.com/google/shlex"
+	"github.com/stretchr/testify/require"
+
+	"vonage-cloud-runtime-cli/testutil"
+	"vonage-cloud-runtime-cli/testutil/mocks"
+)
+
+func TestAppDelete(t *testing.T) {
+	const appID = "12345678-1234-1234-1234-123456789abc"
+
+	type mock struct {
+		DeleteTimes     int
+		DeleteReturnErr error
+		AskYesNoTimes   int
+		AskYesNoReturn  bool
+	}
+	type want struct {
+		errMsg string
+		stdout string
+		stderr string
+	}
+
+	tests := []struct {
+		name string
+		cli  string
+		mock mock
+		want want
+	}{
+		{
+			name: "happy-path-with-yes-flag",
+			cli:  appID + " --yes",
+			mock: mock{
+				DeleteTimes:     1,
+				DeleteReturnErr: nil,
+				AskYesNoTimes:   0,
+			},
+			want: want{
+				stdout: "✓ Application \"" + appID + "\" successfully deleted\n",
+			},
+		},
+		{
+			name: "happy-path-confirm-prompt",
+			cli:  appID,
+			mock: mock{
+				DeleteTimes:     1,
+				DeleteReturnErr: nil,
+				AskYesNoTimes:   1,
+				AskYesNoReturn:  true,
+			},
+			want: want{
+				stdout: "✓ Application \"" + appID + "\" successfully deleted\n",
+			},
+		},
+		{
+			name: "user-aborts-prompt",
+			cli:  appID,
+			mock: mock{
+				DeleteTimes:     0,
+				AskYesNoTimes:   1,
+				AskYesNoReturn:  false,
+			},
+			want: want{
+				stderr: "! Application removal aborted\n",
+			},
+		},
+		{
+			name: "missing-application-id",
+			cli:  "",
+			mock: mock{
+				DeleteTimes:   0,
+				AskYesNoTimes: 0,
+			},
+			want: want{
+				errMsg: "accepts 1 arg(s), received 0",
+			},
+		},
+		{
+			name: "api-error",
+			cli:  appID + " --yes",
+			mock: mock{
+				DeleteTimes:     1,
+				DeleteReturnErr: errors.New("internal server error"),
+			},
+			want: want{
+				errMsg: "failed to delete application: internal server error",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			deploymentMock := mocks.NewMockDeploymentInterface(ctrl)
+			deploymentMock.EXPECT().
+				DeleteVonageApplication(gomock.Any(), appID).
+				Times(tt.mock.DeleteTimes).
+				Return(tt.mock.DeleteReturnErr)
+
+			surveyMock := mocks.NewMockSurveyInterface(ctrl)
+			surveyMock.EXPECT().
+				AskYesNo(gomock.Any()).
+				Times(tt.mock.AskYesNoTimes).
+				Return(tt.mock.AskYesNoReturn)
+
+			ios, _, stdout, stderr := iostreams.Test()
+			ios.SetStdinTTY(true)
+			ios.SetStdoutTTY(true)
+
+			argv, err := shlex.Split(tt.cli)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			f := testutil.DefaultFactoryMock(t, ios, nil, nil, nil, deploymentMock, surveyMock, nil)
+
+			cmd := NewCmdAppDelete(f)
+			cmd.SetArgs(argv)
+			cmd.SetIn(&bytes.Buffer{})
+			cmd.SetOut(io.Discard)
+			cmd.SetErr(io.Discard)
+
+			if _, err := cmd.ExecuteC(); err != nil && tt.want.errMsg != "" {
+				require.Error(t, err, "should throw error")
+				require.Equal(t, tt.want.errMsg, err.Error())
+				return
+			}
+
+			cmdOut := &testutil.CmdOut{
+				OutBuf: stdout,
+				ErrBuf: stderr,
+			}
+
+			if tt.want.stderr != "" {
+				require.Equal(t, tt.want.stderr, cmdOut.Stderr())
+				return
+			}
+			require.NoError(t, err, "should not throw error")
+			require.Equal(t, tt.want.stdout, cmdOut.String())
+		})
+	}
+}
